a332bc9ee078ce56df2fe245ee13326fa75d0524
Al3xiel <gabriel280104casas@gmail.com>
2025-11-11 14:14:21 -0500

    feat(i18n): add translations for task views and update components for localization

--- src/app/tasks/pages/edit-task/edit-task.ts
+++ src/app/tasks/pages/edit-task/edit-task.ts
@@ -7,11 +7,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-edit-task',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatFormFieldModule, MatSelectModule, MatOptionModule, MatButtonModule],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatSelectModule, MatOptionModule, MatButtonModule, TranslatePipe],
   templateUrl: './edit-task.html',
   styleUrls: ['./edit-task.css']
 })
