a9c7b9464e7042b4ca79f8a19c02232a4dea5b59
Neorise456 <defneorise@gmail.com>
2025-12-02 15:27:38 -0500

    fix: update base URL for production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   //http://localhost:8080/api/v1 or https://backend-mm6t.onrender.com/api/v1
-  baseUrl: 'http://localhost:8080/api/v1',
+  baseUrl: 'https://backend-b-xd75.onrender.com/api/v1',
 };
